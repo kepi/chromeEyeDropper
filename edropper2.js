a31d2e68939ba1572712d59a543c128d2286fd48
Kepi <kepi@igloonet.cz>
2010-08-28 22:08:37 +0200

    Fixes and wait cursor

--- edropper2.js
+++ edropper2.js
@@ -7,6 +7,8 @@ var page = {
   canvasBorders: 20,
   canvasData: null,
   dropperActivated: false,
+  screenshoting: false,
+  scrolling: false,
 
   // ---------------------------------
   // MESSAGING 
@@ -44,13 +46,15 @@ var page = {
     page.dropperActivated = true;
     page.screenChanged();
     console.log('activating page dropper');
+    $(document).bind('scrollstart', page.onScrollStart);
+    $(document).bind('scrollstop', page.onScrollStop);
     document.addEventListener("mousemove", page.onMouseMove, false);
     document.addEventListener("click", page.onMouseClick, false);
     document.addEventListener("keydown", page.onKeyDown, false);
-    $(document).bind('scrollstop', page.onScrollStop);
     console.log('activating page dropper done');
 
     $("body").append('<div id="color-tooltip" style="z-index: 1000; width:10px; height: 10px; border: 1px solid #000; display:none; font-size: 15px;"> </div>');
+    $("head").append('<style type="text/css" id="edropper-css">* { cursor: default !important}</style>');
   },
 
   dropperDeactivate: function() {
@@ -63,8 +67,10 @@ var page = {
     document.removeEventListener("click", page.onMouseClick, false);
     document.removeEventListener("keydown", page.onKeyDown, false);
     $(document).unbind('scrollstop', page.onScrollStop);
+    $(document).unbind('scrollstop', page.onScrollStart);
 
     $('#color-tooltip').remove();
+    $("#edropper-css").remove();
   },
 
   // ---------------------------------
@@ -91,9 +97,22 @@ var page = {
   onScrollStop: function() {
     if (!page.dropperActivated)
      return;
+    
+    page.scrolling = false;
 
     console.log("Scroll stop");
     page.screenChanged();
+  },
+
+  onScrollStart: function() {
+    if (!page.dropperActivated)
+     return;
+
+    page.scrolling = true;
+    console.log("Scroll start");
+
+    $('#color-tooltip').hide();
+    $("#edropper-css").html('* { cursor: wait !important}');
   },
 
   onKeyDown: function(e) {
@@ -125,6 +144,9 @@ var page = {
   // ---------------------------------
 
   tooltip: function(e) {
+    if (!page.dropperActivated || page.screenshoting || page.scrolling)
+      return;
+
     var color = page.pickColor(e);
     var fromTop = -15;
     var fromLeft = 10;
@@ -184,12 +206,14 @@ var page = {
   },
 
   // ---------------------------------
-  // CANVAS
+  // UPDATING SCREEN 
   // ---------------------------------
 
   screenChanged: function() {
     if (!page.dropperActivated)
       return;
+
+    page.screenshoting = true;
 
     page.sendMessage({reqtype: 'screenshot', 
       pageWidth: page.width, 
@@ -210,6 +234,10 @@ var page = {
       var context = page.canvas.getContext('2d');
       context.drawImage(image, 0, 0);
       page.canvasData = context.getImageData(0, 0, image.width, image.height).data;
+
+      page.screenshoting = false;
+      $("#edropper-css").html('* { cursor: default !important}');
+      $('#color-tooltip').show();
     }
 
     image.src = page.imageData;
