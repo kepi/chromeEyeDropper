a72945836d652e3a59d2c160bc5c4d88c8f138f4
Kepi <kepi@igloonet.cz>
2011-04-30 20:34:28 +0200

    Rewrite injected helper

--- js/ed_helper.js
+++ js/ed_helper.js
@@ -1,45 +1,43 @@
-var ED_HELPER_VERSION=3;
+var ED_HELPER_VERSION=4;
 
 var edHelper = {
-  version: null,
+  version: ED_HELPER_VERSION,
   options: null,
 
   // listen for incoming messages
-  messageListener: function() {
-    chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
+  messageListener: function(req, sender, sendResponse) {
       switch(req.type) {
-        // ed-helper-options require options in req
-        case 'ed-helper-options':  
-          edHelper.options = req.options; 
-          edHelper.hotKeyStart();
+        case 'helper-version':
+          console.log('helper version ' + edHelper.version);
+          sendResponse({version: edHelper.version, tabid: req.tabid});
           break;
       }
-    });
   },
 
+  // handle hotkeys
   hotKeyStart: function() {
     if ( edHelper.options.hotkeyActivate != null ) {
       // activate web page picker
       shortcut.add(edHelper.options.hotkeyActivate, function() {
-        edHelper.sendMessage({type: "activate2"}, function() {});
+        chrome.extension.sendRequest({type: "activate-from-hotkey"});
       });
     }
   },
 
   // start helper
   init: function() {
-    edHelper.version = ED_HELPER_VERSION;
-    edHelper.messageListener();
+    console.log('init helper version ' + edHelper.version);
     // load options
-    edHelper.sendMessage({type: "ed-helper-options"}, function() {});
-  },
-
-  // helper for sending message to bg
-  sendMessage: function(message) {
-    message.version = edHelper.version;
-    chrome.extension.connect().postMessage(message);
+    chrome.extension.sendRequest({type: "ed-helper-options"}, function(response) {
+      edHelper.options = response.options;
+      edHelper.hotKeyStart();
+    });
   }
 }
 
+// init
 edHelper.init();
 
+// add listener if missing
+if ( !chrome.extension.onRequest.hasListeners() )
+  chrome.extension.onRequest.addListener(function(req, sender, sendResponse) { edHelper.messageListener(req, sender, sendResponse); });
