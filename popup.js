3e756dee9963a3102ff71d7a401c8acb30cb342f
Kepi <kepi@igloonet.cz>
2013-05-05 11:55:04 +0200

    Fix flattr button loading.

--- popup.js
+++ popup.js
@@ -108,6 +108,8 @@ $(document).ready(function() {
         $("#history").html("No history yet. Try to pick some colors from web.");
 
     }
+
+    FlattrLoader.setup();
 });
 
 function setPickerColor(color)
@@ -145,13 +147,3 @@ function showJPicker()
     $('#colorpicker').jPicker({window: {title: '&nbsp;'}, color: {active: active}, images: {clientPath: 'inc/jPicker/images/'}});
 }
 
-/*
-(function() {
- var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
- s.type = 'text/javascript';
- s.async = true;
- s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
- t.parentNode.insertBefore(s, t);
-})();
-
-*/
