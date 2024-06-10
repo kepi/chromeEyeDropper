acfcbb81e8927f422234b88c7342be0487971084
Kepi <kepi@igloonet.cz>
2024-06-10 22:00:18 +0200

    feat: Link component to correctly open new tab with URL
    As we can't open links from extension with normal a href tag, let's
    create new component which will handle this.

---
+++ src/Link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import browser from "webextension-polyfill"
+  export let href: string
+</script>
+
+<a
+  {href}
+  on:click={async (e) => {
+    browser.tabs.create({
+      url: href,
+    })
+    e.preventDefault()
+  }}><slot /></a
+>
