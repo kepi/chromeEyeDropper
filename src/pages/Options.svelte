e0c7c6965b87e9bb0118a6440b6c4877b43870a8
Kepi <kepi@igloonet.cz>
2024-06-10 20:50:35 +0200

    feat(options): working options page

--- src/pages/Options.svelte
+++ src/pages/Options.svelte
@@ -3,7 +3,17 @@
   import ColorBox from "../lib/ColorBox.svelte"
   import PickFromWebpageButton from "../lib/PickFromWebpageButton.svelte"
   import { random } from "@ctrl/tinycolor"
-  import { autoClipboard } from "../store"
+  import {
+    autoClipboard,
+    autoClipboardType,
+    enableColorToolbox,
+    enableColorTooltip,
+    enableRightClickDeactivate,
+    dropperCursor,
+  } from "../store"
+  import cursorDefault from "/img/cursor_default.png"
+  import cursorCrosshair from "/img/cursor_crosshair.png"
+  import Link from "../Link.svelte"
 </script>
 
 <div class="navbar bg-base-100">
@@ -28,21 +38,17 @@
         tabindex="0"
         class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
       >
-        <li><a>Options</a></li>
-        <li><a>Palettes</a></li>
+        <li><a href="#">Options</a></li>
       </ul>
     </div>
     <a class="btn btn-ghost text-xl">Eye Dropper</a>
   </div>
   <div class="navbar-center hidden lg:flex">
     <ul class="menu menu-horizontal px-1">
-      <li><a>Options</a></li>
-      <li><a>Palettes</a></li>
+      <li><a href="#">Options</a></li>
     </ul>
   </div>
-  <div class="navbar-end">
-    <a class="btn">About</a>
-  </div>
+  <div class="navbar-end"></div>
 </div>
 
 <div>
@@ -51,7 +57,7 @@
 
   <div class="w-max px-6">
     <div class="form-control">
-      <label class="label cursor-pointer">
+      <label class="label justify-start cursor-pointer">
         <input
           type="checkbox"
           name="autoClipboard"
@@ -61,16 +67,31 @@
         <span class="label-text">Copy color to clipboard</span>
       </label>
 
-      <div class="pl-6">
-        <label class="label cursor-pointer">
-          <input type="radio" name="radio-2" class="radio radio-primary mr-3" checked />
+      <div class="pl-12">
+        <label class="label justify-start cursor-pointer">
+          <input
+            type="radio"
+            name="autoClipboardType"
+            value="#rgbhex"
+            bind:group={$autoClipboardType}
+            disabled={!$autoClipboard}
+            class="radio radio-primary mr-3"
+          />
           <span class="label-text"
-            ># RGB Hex color<code class="badge badge-accent">#aabbcc</code></span
+            ># RGB Hex color <code class="badge badge-accent">#aabbcc</code></span
           >
         </label>
-        <label class="label cursor-pointer">
-          <input type="radio" name="radio-2" class="radio radio-primary mr-3" checked />
-          <span class="label-text">RGB Hex color<code class="badge badge-accent">aabbcc</code></span
+        <label class="label justify-start cursor-pointer">
+          <input
+            type="radio"
+            name="autoClipboardType"
+            value="rgbhex"
+            bind:group={$autoClipboardType}
+            disabled={!$autoClipboard}
+            class="radio radio-primary mr-3"
+          />
+          <span class="label-text"
+            >RGB Hex color <code class="badge badge-accent">aabbcc</code></span
           >
         </label>
       </div>
@@ -78,30 +99,89 @@
   </div>
 
   <h2 class="text-2xl px-6 py-2 my-2 bg-primary text-black">Keyboard shortcuts</h2>
-  <div class="w-max px-6">
-    <div>
+  <div class="px-6">
+    <div class="pb-3 border-b border-b-primary mb-3">
       <span class="text-xl">Pick color from current webpage:</span>
-      <span class="text-xl kbd">Alt+P</span>
+      <span class="text-xl ml-2 kbd">Alt+P</span>
       <p class="prose">
-        You can change this shortcut at <a href="chrome://extensions/shortcuts"
-          >Chrome's Extensions Keyboard Shortcut page</a
+        You can change this shortcut at <Link href="chrome://extensions/shortcuts"
+          >Chrome's Extensions Keyboard Shortcut page</Link
         >.
       </p>
     </div>
-    <div>
+    <div class>
       <span class="text-xl">Cancel Pick:</span>
-      <span class="kbd text-xl">Esc</span>
+      <span class="kbd ml-2 text-xl">Esc</span>
       <p class="prose">
-        Simply cancel active picking color from web page. No color will be stored in history.
+        When are you in process of picking color from a web page, you can cancel it with Esc. No
+        color will be stored in history.
       </p>
 
       <div class="pl-6 form-control">
-        <label class="label cursor-pointer">
-          <input type="checkbox" checked="checked" class="checkbox checkbox-primary mr-3" />
+        <label class="label justify-start cursor-pointer">
+          <input
+            type="checkbox"
+            bind:checked={$enableRightClickDeactivate}
+            class="checkbox checkbox-primary mr-3"
+          />
           <span class="label-text">also cancel with <span class="kbd">Mouse Right Click</span></span
           >
         </label>
       </div>
     </div>
+  </div>
+
+  <h2 class="text-2xl px-6 py-2 my-2 bg-primary text-black">Pick from Web Page</h2>
+  <div class="px-6 form-control">
+    <div class="pb-3 border-b border-b-primary mb-3">
+      <div class="text-xl pb-2">Features</div>
+      <label class="label justify-start cursor-pointer">
+        <input
+          type="checkbox"
+          name="autoClipboard"
+          bind:checked={$enableColorToolbox}
+          class="checkbox checkbox-primary mr-3"
+        />
+        <span class="label-text">Enable box with color information in right bottom corner.</span>
+      </label>
+      <label class="label justify-start cursor-pointer">
+        <input
+          type="checkbox"
+          name="autoClipboard"
+          bind:checked={$enableColorTooltip}
+          class="checkbox checkbox-primary mr-3"
+        />
+        <span class="label-text">Enable color tooltip next to the mouse cursor.</span>
+      </label>
+    </div>
+    <div>
+      <div class="text-xl">Eye Dropper Cursor</div>
+      <p class="text-base py-2 italic">
+        Hover over option text to see how it really would look like on your system.
+      </p>
+      <label class="label justify-start cursor-default">
+        <input
+          type="radio"
+          name="dropperCursor"
+          value="default"
+          bind:group={$dropperCursor}
+          class="radio radio-primary mr-3"
+        />
+        <span class="label-text flex gap-2"><img alt="default" src={cursorDefault} /> default</span>
+      </label>
+      <label class="label justify-start cursor-crosshair">
+        <input
+          type="radio"
+          name="dropperCursor"
+          value="crosshair"
+          bind:group={$dropperCursor}
+          class="radio radio-primary mr-3"
+        />
+        <span class="label-text flex gap-2"
+          ><img alt="crosshair" src={cursorCrosshair} /> crosshair</span
+        >
+      </label>
+    </div>
+    <div class></div>
   </div>
 </div>
