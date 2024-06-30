128c20c2e375e74ef562f85e3c0fcc5b0521397f
Kepi <kepi@igloonet.cz>
2024-06-30 18:21:18 +0200

    feat: working deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,26 @@ import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import webExtension, { readJsonFile } from "vite-plugin-web-extension"
 
+const beta = process.env.BETA === "1" ? true : false
+
 function generateManifest() {
   const manifest = readJsonFile("src/manifest.json")
   const pkg = readJsonFile("package.json")
-  return {
-    name: pkg.name,
-    description: pkg.description,
-    version: pkg.version,
-    ...manifest,
+
+  if (beta) {
+    return {
+      name: `${pkg.name} BETA`,
+      description: "THIS EXTENSION IS FOR BETA TESTING",
+      version: `${pkg.version}`,
+      ...manifest,
+    }
+  } else {
+    return {
+      name: pkg.name,
+      description: pkg.description,
+      version: pkg.version,
+      ...manifest,
+    }
   }
 }
 
