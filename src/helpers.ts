0e6997734f33a741d361ff0bdcad5e23507c0e6d
Kepi <kepi@igloonet.cz>
2024-10-27 18:54:22 +0100

    feat: add toString helper function

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,6 @@
 // module "helpers"
+import { match, P } from "ts-pattern"
+
 interface ElementAttributes {
   [key: string]: string
 }
@@ -11,4 +13,11 @@ function createNode(node: string, attributes: ElementAttributes): HTMLElement {
   return el
 }
 
-export { createNode }
+const toString = (value: unknown): string =>
+  match(value)
+    .with(P.string, (str) => str)
+    .with(P.number, (num) => num.toFixed(2))
+    .with(P.boolean, (bool) => `${bool}`)
+    .otherwise(() => "Unknown")
+
+export { createNode, toString }
