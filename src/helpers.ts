ca51969d00fe636faf90834c20373c8dc1b2b420
Kepi <kepi@igloonet.cz>
2024-11-10 16:39:56 +0100

    feat: handle more pages with disabled picker
    And support special pages and protocols for all planned browsers.

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,7 +5,7 @@ interface ElementAttributes {
   [key: string]: string
 }
 
-function createNode(node: string, attributes: ElementAttributes): HTMLElement {
+export function createNode(node: string, attributes: ElementAttributes): HTMLElement {
   const el = document.createElement(node)
   for (let key in attributes) {
     el.setAttribute(key, attributes[key])
@@ -13,11 +13,78 @@ function createNode(node: string, attributes: ElementAttributes): HTMLElement {
   return el
 }
 
-const toString = (value: unknown): string =>
+export const toString = (value: unknown): string =>
   match(value)
     .with(P.string, (str) => str)
     .with(P.number, (num) => num.toFixed(2))
     .with(P.boolean, (bool) => `${bool}`)
     .otherwise(() => "Unknown")
 
-export { createNode, toString }
+export const capitalize = (str: string = "", lowerRest = false): Capitalize<string> =>
+  (str.slice(0, 1).toUpperCase() +
+    (lowerRest ? str.slice(1).toLowerCase() : str.slice(1))) as Capitalize<string>
+
+function getRestrictedProtocols(browser: string) {
+  const firefoxProtos = ["about", "moz", "resource", "chrome", "jar"]
+
+  const chromeProtos = ["chrome", "data", "devtools"]
+
+  const edgeProtos = chromeProtos.concat("edge")
+
+  if (browser === "firefox") {
+    return firefoxProtos
+  } else if (browser === "edge") {
+    return edgeProtos
+  }
+
+  return chromeProtos
+}
+
+export function isProtoRestricted(protocol: string, browser = import.meta.env.BROWSER) {
+  const restrictedProtocols = getRestrictedProtocols(browser)
+
+  for (let restrictedProtocol of restrictedProtocols) {
+    if (protocol.startsWith(restrictedProtocol)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+export function isSiteRestricted(url: URL, browser = import.meta.env.BROWSER) {
+  const firefoxSites = [
+    "accounts-static.cdn.mozilla.net",
+    "accounts.firefox.com",
+    "addons.cdn.mozilla.net",
+    "addons.mozilla.org",
+    "api.accounts.firefox.com",
+    "content.cdn.mozilla.net",
+    "discovery.addons.mozilla.org",
+    "install.mozilla.org",
+    "oauth.accounts.firefox.com",
+    "profile.accounts.firefox.com",
+    "support.mozilla.org",
+    "sync.services.mozilla.com",
+  ]
+
+  const chromeURLs = ["https://chrome.google.com/webstore", "https://chromewebstore.google.com"]
+
+  const edgeURLs = chromeURLs.concat("https://microsoftedge.microsoft.com/addons")
+
+  if (browser === "firefox") {
+    for (let restrictedSite of firefoxSites) {
+      if (url.hostname === restrictedSite) {
+        return true
+      }
+    }
+  } else {
+    const urls = browser === "edge" ? edgeURLs : chromeURLs
+
+    for (let restrictedURL of urls) {
+      if (url.toString().startsWith(restrictedURL)) {
+        return true
+      }
+    }
+  }
+}
--- src/lib/PickFromWebpageButton.svelte
+++ src/lib/PickFromWebpageButton.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
+  import { capitalize, isProtoRestricted, isSiteRestricted } from "@/helpers"
   import browser from "webextension-polyfill"
   import { getTab } from "~/entrypoints/background"
   import PickFromWpButton from "~/lib/PickFromWpButton.svelte"
 
-  const isChrome = import.meta.env.CHROME || import.meta.env.OPERA
-  const isFirefox = import.meta.env.FIREFOX
-  const isEdge = import.meta.env.EDGE
+  const browserName = capitalize(import.meta.env.BROWSER)
 </script>
 
 {#await getTab() then tab}
@@ -13,44 +12,49 @@
     <PickFromWpButton
       reason="Can't pick from this page. Your browser won't allow me to access it."
     />
+  {:else if tab.url === "about:blank"}
+    <PickFromWpButton
+      reason="Can't pick from new blank page. Your browser won't allow me to access it."
+    />
   {:else if tab.url === browser.runtime.getURL("/popup.html")}
     <div class="-mt-6"></div>
-  {:else if isChrome && tab.url.startsWith("chrome")}
-    <PickFromWpButton reason="Extensions are not allowed to interact with special browser pages." />
-  {:else if isEdge && tab.url.startsWith("edge")}
-    <PickFromWpButton reason="Extensions are not allowed to interact with edge:" />
-  {:else if isFirefox && tab.url.startsWith("about:")}
-    <PickFromWpButton reason="Extensions are not allowed to interact with about: pages." />
-  {:else if isChrome && tab.url.startsWith("https://chromewebstore.google.com")}
-    <PickFromWpButton
-      reason="Chrome extensions are not allowed to interact with Chrome Web Store."
-    />
-  {:else if isFirefox && tab.url.startsWith("https://addons.mozilla.org")}
-    <PickFromWpButton
-      reason="Firefox extensions are not allowed to interact with Mozilla Addons page."
-    />
-  {:else if tab.url.startsWith("data:image")}
-    <PickFromWpButton
-      reason="Root of the page is inline data image and I can't interact with it yet."
-      issueId={264}
-    />
-  {:else if tab.url.endsWith(".svg")}
-    <PickFromWpButton
-      reason="Root of the page is SVG image and I can't interact with it yet."
-      issueId={213}
-    />
-  {:else if tab.url.indexOf("file") === 0}
-    {#await browser.extension.isAllowedFileSchemeAccess() then isAllowedAccess}
-      {#if isAllowedAccess}
-        <PickFromWpButton tabId={tab.id} />
-      {:else}
-        <PickFromWpButton
-          reason="You have to allow access to file URL's first if you want to pick color from local files."
-          href="https://eyedropper.org/help/file-urls"
-        />
-      {/if}
-    {/await}
-  {:else if tab.id !== undefined}
-    <PickFromWpButton tabId={tab.id} />
+  {:else}
+    {@const url = new URL(tab.url)}
+    {#if url.protocol === "view-source"}
+      <PickFromWpButton
+        reason="Can't pick form source view page. Your browser won't allow me to access it."
+      />
+    {:else if url.protocol === "file"}
+      {#await browser.extension.isAllowedFileSchemeAccess() then isAllowedAccess}
+        {#if isAllowedAccess}
+          <PickFromWpButton tabId={tab.id} />
+        {:else}
+          <PickFromWpButton
+            reason="You have to allow access to file URL's first if you want to pick color from local files."
+            href="https://eyedropper.org/help/file-urls"
+          />
+        {/if}
+      {/await}
+    {:else if isProtoRestricted(url.protocol)}
+      <PickFromWpButton
+        reason="{browserName} extensions are not allowed to interact with {url.protocol} pages."
+      />
+    {:else if isSiteRestricted(url)}
+      <PickFromWpButton
+        reason="{browserName} extensions are not allowed to interact with restricted sites."
+      />
+    {:else if tab.url.startsWith("data:image")}
+      <PickFromWpButton
+        reason="Root of the page is inline data image and I can't interact with it yet."
+        issueId={264}
+      />
+    {:else if tab.url.endsWith(".svg")}
+      <PickFromWpButton
+        reason="Root of the page is SVG image and I can't interact with it yet."
+        issueId={213}
+      />
+    {:else if tab.id !== undefined}
+      <PickFromWpButton tabId={tab.id} />
+    {/if}
   {/if}
 {/await}
