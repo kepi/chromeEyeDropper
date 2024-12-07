a28d66b4c864434181d1b140be54c197f6318720
Kepi <kepi@igloonet.cz>
2024-12-07 18:11:31 +0100

    fix(popup): back to palettes after delete

--- src/lib/PaletteDelete.svelte
+++ src/lib/PaletteDelete.svelte
@@ -8,10 +8,10 @@
 
   let { paletteId = $bindable(), show = $bindable() }: Props = $props()
 
-  function reallyDeletePalette(id: number, show: boolean) {
+  function reallyDeletePalette() {
     // we have to use pStore function as we need to remove it first from store,
     // than from synced store
-    pStore.destroyPalette(id)
+    pStore.destroyPalette(paletteId)
 
     // reset id and close dialog
     paletteId = -1
@@ -19,13 +19,13 @@
   }
 </script>
 
-{#if paletteId > -1}
+{#if paletteId > -1 && show}
   <h4>
     Really delete palette "<span class="font-mono"
       >#{$pStore[paletteId].id} {$pStore[paletteId].name}"?</span
     >
   </h4>
-  <button class="btn btn-error" onclick={() => reallyDeletePalette(paletteId, show)}
+  <button class="btn btn-error" onclick={() => reallyDeletePalette()}
     >Yes, I know it can't be restored.</button
   >
 {/if}
